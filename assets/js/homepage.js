f6f0f5fe09d789bf6dadddb540946bd3568a5c25
MWPX777 <mwpx777@gmail.com>
2021-01-15 10:58:49 -0600

    finished module 6.1

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,6 +1,26 @@
-function getUsersRepos() {
-    fetch("http://api.github.com/users/octocat/repos");
-    console.log('function was called')
+// function getUsersRepos(user) {
+//     // this function will fetch the repos from api.github
+//      fetch("http://api.github.com/users/octocat/repos").then(function(response){
+//      //.json method will convert the response into json
+//     response.json().then(function(data){
+//      console.log(data);
+//     //  this will log an array into the console
+// });
+// });
+// }
+// getUsersRepos();
+
+function getUsersRepos(user){
+    //format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiUrl).then(function(response){
+        response.json().then(function(data){
+            console.log(data);
+        });
+    });
 };
 
-getUsersRepos();
+// argument in this function call will get passed to 'user' in function above
+getUsersRepos("twitter")
