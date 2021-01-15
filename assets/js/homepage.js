d2ed2de8ea328eeeae119759d67f495eaba55ffb
MWPX777 <mwpx777@gmail.com>
2021-01-15 13:18:24 -0600

    added username search form

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -10,17 +10,39 @@
 // }
 // getUsersRepos();
 
+let userFormEl = document.querySelector('#user-form');
+let nameInputEL = document.querySelector('#username');
+
+userFormEl.addEventListener('submit', formSubmitHandler)
+
 function getUsersRepos(user){
     //format the github api url
+    // example "http://api.girhub.com/users/mwpx777/repos"
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
     // make a request to the url
     fetch(apiUrl).then(function(response){
         response.json().then(function(data){
             console.log(data);
+            console.log(apiUrl);
         });
     });
 };
 
+function formSubmitHandler(event){
+    event.preventDefault();
+    // get value from input element
+    var username = nameInputEL.value.trim();
+    if (username){
+        //this will pass 'username' into getUsersRepos function above
+        getUsersRepos(username);
+        // this clears the nameInputEl 
+        nameInputEL.value = "";
+    }else{
+        alert("Please enter GitHub username");
+    }
+
+};
+
 // argument in this function call will get passed to 'user' in function above
-getUsersRepos("twitter")
+getUsersRepos()
