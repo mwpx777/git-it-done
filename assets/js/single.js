4fad19c7983510bcec7b1af9213bb7b927e3428a
MWPX777 <mwpx777@gmail.com>
2021-01-16 09:35:23 -0600

    added show issues page

---
+++ assets/js/single.js
@@ -0,0 +1,51 @@
+var issuesContainerEl = document.querySelector('#issues-container');
+
+function getRepoIssues(repo){
+    
+    var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+    fetch(apiUrl).then(function(response){
+        // request was successful
+        if(response.ok){
+        response.json().then(function(data){
+            displayIssues(data);
+        });
+    }else{
+        alert("There was a problem with your request!");
+    }
+    })
+};
+
+function displayIssues(issues){
+    // check for empty repo issues
+    if(issues.length === 0){
+        issuesContainerEl.textContent = "This repo has no open issues!";
+        return;
+    }
+
+    for(var i=0; i<issues.length; i++){
+        // create a link element to take users to the issue on github
+        var issueEl = document.createElement('a');
+        issueEl.classList = "list-item flex-row justify-space-between align-center";
+        issueEl.setAttribute('href', issues[i].html_url);
+        issueEl.setAttribute('target', "_blank")
+        //create span to hold issue title
+        var titleEl = document.createElement('span');
+        titleEl.textContent = issues[i].title;
+        // append titleEl to issueEl container
+        issueEl.appendChild(titleEl);
+        // create a type element
+        var typeEl = document.createElement('span');
+        // check if issue is actual issue or pull request
+        if(issues[i].pull_request){
+            typeEl.textContent="(Pull Request)"
+        }else{
+            typeEl.textContent="(Issue)"
+        }
+        // append  typeEl to issuesEl Container
+        issueEl.appendChild(typeEl);
+        // append issuesEl to issuesContainerEl
+        issuesContainerEl.appendChild(issueEl);
+    }
+
+};
+getRepoIssues("mwpx777/git-it-done")
